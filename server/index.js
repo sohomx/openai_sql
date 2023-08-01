1c90a890b41aca10fcf807b251568c8faefa9167
Sohom Pal <sohom377@gmail.com>
2023-08-01 12:11:36 +0530

    successful post request for queryDescription

--- server/index.js
+++ server/index.js
@@ -2,13 +2,20 @@
  import cors from "cors"
 
  const app = express();
-
+// parse json inside the body
+app.use(express.json())
  app.use(cors());
 
  const port = process.env.PORT || 3005;
 
  app.get("/", (req, res) => {
     res.send("Hello world from our API")
+ })
+
+ app.post("/generate", (req, res) => {
+    const queryDescription = req.body.queryDescription
+    console.log("received description: ", queryDescription)
+    res.json({ response: `you sent this: ${queryDescription}`})
  })
 
  app.listen(port, () => {
